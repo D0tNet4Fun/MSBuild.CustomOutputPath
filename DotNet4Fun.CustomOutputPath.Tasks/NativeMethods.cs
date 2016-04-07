c33fa2ea3492ec29163d10a5f06430021abc0797
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2016-04-07 23:16:29 +0300

    RedirectBin: detect if the default target dir is a symlink which does not point to the custom target dir and if so then delete it

--- DotNet4Fun.CustomOutputPath.Tasks/NativeMethods.cs
+++ DotNet4Fun.CustomOutputPath.Tasks/NativeMethods.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
 
 namespace DotNet4Fun.CustomOutputPath.Tasks
 {
@@ -10,6 +14,59 @@ namespace DotNet4Fun.CustomOutputPath.Tasks
             [In] string targetFileName,
             SymlinkTargetType flags
             );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern SafeFileHandle CreateFile(
+            string fileName,
+            [MarshalAs(UnmanagedType.U4)] FileAccessEx fileAccess,
+            [MarshalAs(UnmanagedType.U4)] FileShare fileShare,
+            IntPtr securityAttributes,
+            [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+            FileOptions flags,
+            IntPtr template
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeviceIoControl(
+            SafeFileHandle hDevice,
+            int dwIoControlCode,
+            [MarshalAs(UnmanagedType.AsAny)] [In] object inBuffer,
+            int nInBufferSize,
+            [MarshalAs(UnmanagedType.AsAny)] [Out] object outBuffer,
+            int nOutBufferSize,
+            ref int pBytesReturned,
+            IntPtr lpOverlapped
+            );
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class ReparseDataBuffer
+        {
+            public uint ReparseTag;
+            public ushort ReparseDataLength;
+            public ushort Reserved;
+            public ushort SubstituteNameOffset;
+            public ushort SubstituteNameLength;
+            public ushort PrintNameOffset;
+            public ushort PrintNameLength;
+            public uint Flags;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32767)]
+            public byte[] PathBuffer;
+
+            public string GetTargetName()
+            {
+                return Encoding.Unicode.GetString(PathBuffer, PrintNameOffset, PrintNameLength);
+            }
+        }
+
+        [Flags]
+        public enum FileAccessEx : uint
+        {
+            None = 0x0,
+            GenericRead = 0x80000000,
+            GenericWrite = 0x40000000,
+            GenericReadWrite = GenericRead | GenericWrite
+        }
     }
 
     internal enum SymlinkTargetType
---
+++ DotNet4Fun.CustomOutputPath.Tasks/Tasks/QuerySymlink.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace DotNet4Fun.CustomOutputPath.Tasks.Tasks
+{
+    public class QuerySymlink : Task
+    {
+        [Required]
+        public string LinkName { get; set; }
+
+        [Output]
+        public string TargetName { get; set; }
+
+        [Output]
+        public string TargetType { get; set; }
+
+        public override bool Execute()
+        {
+            var reparseDataBuffer = GetReparsePointInfo();
+            if (reparseDataBuffer != null)
+            {
+                TargetName = reparseDataBuffer.GetTargetName();
+            }
+            if (TargetName != null)
+            {
+                try
+                {
+                    var attributes = File.GetAttributes(TargetName);
+                    var targetType = (attributes & FileAttributes.Directory) == FileAttributes.Directory ? SymlinkTargetType.Directory : SymlinkTargetType.File;
+                    TargetType = targetType.ToString();
+                }
+                catch (FileNotFoundException)
+                {
+                    // ignore
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    // ignore
+                }
+            }
+            return true;
+        }
+
+        private NativeMethods.ReparseDataBuffer GetReparsePointInfo()
+        {
+            const uint fileOpenReparsePoint = 0x00200000;
+            const uint fileFlagBackupSemantics = 0x02000000;
+            const FileOptions flags = (FileOptions)(fileOpenReparsePoint | fileFlagBackupSemantics);
+            using (var handle = NativeMethods.CreateFile(LinkName, NativeMethods.FileAccessEx.None, FileShare.Read, IntPtr.Zero, FileMode.Open, flags, IntPtr.Zero))
+            {
+                if (handle.IsInvalid)
+                {
+                    throw Win32Utils.GetExceptionForLastError();
+                }
+
+                var reparseDataBuffer = new NativeMethods.ReparseDataBuffer();
+
+                const int fsctlGetReparsePoint = 0x000900A8;
+                var bytesReturned = 0;
+                var success = NativeMethods.DeviceIoControl(handle, fsctlGetReparsePoint,
+                    IntPtr.Zero, 0,
+                    reparseDataBuffer, Marshal.SizeOf(typeof(NativeMethods.ReparseDataBuffer)),
+                    ref bytesReturned, IntPtr.Zero);
+
+                if (!success)
+                {
+                    const uint pathNotAReparsePointError = 0x80071126;
+                    if ((uint)Marshal.GetHRForLastWin32Error() == pathNotAReparsePointError)
+                    {
+                        return null;
+                    }
+                    throw Win32Utils.GetExceptionForLastError();
+                }
+                return reparseDataBuffer;
+            }
+        }
+    }
+}
