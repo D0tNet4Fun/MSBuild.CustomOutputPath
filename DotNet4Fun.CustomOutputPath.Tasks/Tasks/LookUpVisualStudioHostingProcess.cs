25d8555bb414cdc33ba9084e75f3a72f40bb35ce
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2016-04-08 20:28:38 +0300

    RedirectBin: fail build if the project is an EXE which uses the VS hosting processes for debugging, and the process is running

---
+++ DotNet4Fun.CustomOutputPath.Tasks/Tasks/LookUpVisualStudioHostingProcess.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace DotNet4Fun.CustomOutputPath.Tasks.Tasks
+{
+    public class LookUpVisualStudioHostingProcess : Task
+    {
+        [Required]
+        public string TargetName { get; set; }
+
+        [Output]
+        public bool IsRunning { get; private set; }
+
+        public override bool Execute()
+        {
+            var processes = GetVisualStudioHostingProcesses();
+            try
+            {
+                IsRunning = processes.Length > 0;
+                if (IsRunning)
+                {
+                    Log.LogMessage(MessageImportance.Normal, $"Found {processes.Length} VS hosting processes for project {TargetName}.");
+                }
+            }
+            finally
+            {
+                foreach (var process in processes)
+                {
+                    process.Dispose();
+                }
+            }
+            return true;
+        }
+
+        private Process[] GetVisualStudioHostingProcesses()
+        {
+            var processName = $"{TargetName}.vshost";
+
+            Log.LogMessage(MessageImportance.Normal, $"Finding Visual Studio hosting process for {TargetName} ({processName})");
+            var processes = Process.GetProcessesByName(processName);
+            return processes;
+        }
+    }
+}
